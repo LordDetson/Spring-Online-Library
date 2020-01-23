14f833b5fefa4f9230cd758d59f5f7076d1a4be0
Babanin Dmitry <babanin.dima@gmail.com>
2020-01-23 15:27:08 +0300

    doc: changed project configuration to deploy on the application server

--- src/main/java/by/babanin/booklibrary/BookLibraryApplication.java
+++ src/main/java/by/babanin/booklibrary/BookLibraryApplication.java
@@ -1,13 +1,13 @@
 package by.babanin.booklibrary;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookLibraryApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(BookLibraryApplication.class, args);
+public class BookLibraryApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BookLibraryApplication.class);
     }
-
 }
