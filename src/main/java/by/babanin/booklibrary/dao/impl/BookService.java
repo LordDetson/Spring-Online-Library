30a3fbe4988c616ec0a77613b2cd7b5bbb1970c2
Babanin Dmitry <babanin.dima@gmail.com>
2020-01-23 14:05:53 +0300

    refactor: added books sorting

--- src/main/java/by/babanin/booklibrary/dao/impl/BookService.java
+++ src/main/java/by/babanin/booklibrary/dao/impl/BookService.java
@@ -23,7 +23,7 @@ public class BookService implements BookDao {
 
     @Override
     public List<Book> findTopBooks(int limit) {
-        return bookRepository.findTopBooks(PageRequest.of(0, limit)).getContent();
+        return bookRepository.findTopBooks(PageRequest.of(0, limit, Sort.by(Sort.Direction.ASC, "name"))).getContent();
     }
 
     @Override
