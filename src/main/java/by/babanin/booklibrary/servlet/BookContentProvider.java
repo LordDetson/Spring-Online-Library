db2ee1e0f039cf2082ba8512cba33ae6372993e7
Babanin Dmitry <babanin.dima@gmail.com>
2020-01-24 16:02:46 +0300

    feat: book reading added

---
+++ src/main/java/by/babanin/booklibrary/servlet/BookContentProvider.java
@@ -0,0 +1,37 @@
+package by.babanin.booklibrary.servlet;
+
+import by.babanin.booklibrary.dao.BookDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BookContentProvider extends HttpServlet {
+    private ApplicationContext context;
+
+    @Override
+    public void init() throws ServletException {
+        context = WebApplicationContextUtils.getRequiredWebApplicationContext(this.getServletContext());
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (ServletOutputStream out = resp.getOutputStream()) {
+            Long id = Long.valueOf(req.getParameter("id"));
+            long viewCount = Long.parseLong(req.getParameter("viewCount"));
+            BookDao bookDao = context.getBean(BookDao.class);
+            byte[] content = bookDao.getContent(id);
+            bookDao.updateViewCount(viewCount + 1, id);
+            resp.setContentType("application/pdf");
+            resp.setContentLength(content.length);
+            out.write(content);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
